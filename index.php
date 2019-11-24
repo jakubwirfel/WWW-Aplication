eff832bbf412b7b8c5c3be43b784d3a344d3b5e2
jakubwirfel <jakubwirfel@gmail.com>
2019-11-24 21:54:27 +0100

    Hero image created

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,800&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <main class="main_page">
+            <nav class="navigation">
+                
+            </nav>
+            <div class="hero_box">
+                <div class="hero_text">                
+                    <h1>
+                        Modern villa <br>to rent for you
+                    </h1>
+                </div>
+                <a class="hero_button" href="#">
+                    Rent Now
+                </a>
+            </div>
+        </main>
+    </body>
+</html>
