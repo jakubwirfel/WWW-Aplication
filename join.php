03244d5293c18e85fb12a744a4de5185f4eebef1
jakubwirfel <jakubwirfel@gmail.com>
2019-11-26 16:15:55 +0100

    Added sass files and nav in single file

--- join.php
+++ join.php
@@ -3,25 +3,15 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Villa Rent</title>
+        <title>Join Villa Rent</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="./css/main.css">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,800&display=swap" rel="stylesheet">
     </head>
-    <body class="home_body">
+    <body class="body_join">
         <main class="main_page">
             <?php include "nav.php"?>
-            <div class="hero_box">
-                <div class="hero_text">                
-                    <h1>
-                        Modern villa <br>to rent for you
-                    </h1>
-                </div>
-                <a class="hero_button" href="#">
-                    Rent Now
-                </a>
-            </div>
         </main>
     </body>
 </html>
---
+++ nav.php
@@ -0,0 +1,38 @@
+<header>
+            <a href="index.php"><div class="logo"><img src="assets/logo.png"/></div></a>
+                <nav class="navigation">
+                    <ul>
+                        <li><a href="#open-modal">Sign in</a></li>
+                        <li><a href="join.php">Sign up</a></li>
+                    </ul>
+                </nav>
+                <div class="hamburger">
+                    <div class="line line1"></div>
+                    <div class="line line2"></div>
+                    <div class="line line3"></div>
+                </div>
+            </header>
+            <div id="open-modal" class="modal-window">
+                <div>
+                    <a href="#" title="Close" class="modal-close">Close</a>
+                    <div class="modal-header">
+                        <h1>Login</h1>
+                    </div>  
+                        <form action="/" method="post" class="form_login">
+                            <label class="label_login">
+                                Email Address
+                            </label>
+                            <div class="field-wrap_login">
+                                <input class="input_login" type="email" required autocomplete="off"/>
+                            </div>
+                            <label class="label_login">
+                                Password
+                            </label>
+                            <div class="field-wrap">
+                                <input class="input_login" type="password" required autocomplete="off"/>
+                            </div>
+                            <p class="forgot_login"><a href="#">Forgot Password?</a></p>
+                            <button class="button_login">Log In</button>
+                        </form>
+                </div>
+            </div>
