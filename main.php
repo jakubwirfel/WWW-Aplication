61c8152ce1433876c1a594c1d3bc63fdccd6b951
jakubwirfel <jakubwirfel@gmail.com>
2019-12-10 20:25:25 +0100

    Card hotel fetch from db - work

--- main.php
+++ main.php
@@ -87,63 +87,56 @@
             <section class="map" id="map">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d65178314.71238159!2d12.084603761196673!3d4.379495949476939!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spl!2spl!4v1575569956335!5m2!1spl!2spl" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
             </section>
-            <section class="hotels grid_box">
+            <section class="hotels grid_box"> 
+            <?php 
+                $query = "SELECT * FROM hotels";
+                $results = mysqli_query($db, $query);
+                $query_img = "SELECT * FROM hotels_img";
+                $results_img = mysqli_query($db, $query_img);
+                while ($row = mysqli_fetch_array($results) ) { 
+                    $row_img = mysqli_fetch_array($results_img) 
+            ?>
                 <article>
                     <figure class="img_hotel">
-                        <img src="./hotels_img/villa-perfect-1.jpg"/>
+                        <img src="<?php echo $row_img['url']?>"/>
                     </figure>
                     <div class="hotel_box">
                         <div class="hotel_location">
-                            <p>Hungary Dalmatia</p> | <p>Coast</p>
+                            <p><?php echo $row['location']?></p> | <p><?php echo $row['place']?></p>
                         </div>
                         <div class="hotel_name">
-                            <h1>Villa Perfect</h1>
+                            <h1><?php echo $row['name']?></h1>
                         </div>
                         <div class="hotel_info_box">
                             <div class="hotel_area">
                                 <div>
                                     <img src="./assets/living_area.svg" alt="living area"/>
-                                    <span>Living area: 100 m2</span>
+                                    <span>Living area: <?php echo $row['l_area']?></span>
                                 </div>
                                 <div>
                                     <img src="./assets/persons.svg" alt="persons"/>
-                                    <span>Persons: 9</span>
+                                    <span>Persons: <?php echo $row['persons']?></span>
                                 </div>
                                 <div>
                                     <img src="./assets/bedrooms.svg" alt="bedrooms"/>
-                                    <span>Bedrooms: 4</span>
+                                    <span>Bedrooms: <?php echo $row['bedrooms']?></span>
                                 </div>
                                 <div>
                                     <img src="./assets/bathrooms.svg" alt="bathrooms"/>
-                                    <span>Bathrooms: 4</span>
+                                    <span>Bathrooms: <?php echo $row['bathrooms']?></span>
                                 </div>
                             </div>
                             <div class="hotel_price">
                                 <h1>FROM</h1>
-                                    <span class="daily_contet">350$</span>
+                                    <span class="daily_contet"><?php echo $row['price']?>$</span>
                                     <span>DAILY</span>
-                                    <span class="weekly_contet">2.450$</span>
+                                    <span class="weekly_contet"><?php echo $row['price']*7?>$</span>
                                     <span>WEEKLY</span>
                             </div>
                         </div>
                     </div>
                 </article>
-                <article>2</article>
-                <article>3</article>
-                <article>4</article>
-                <article>5</article>
-                <article>6</article>
-                <article>7</article>
-                <article>8</article>
-                <article>9</article>
-                <article>9</article>
-                <article>9</article>
-                <article>9</article>
-                <article>9</article>
-                <article>9</article>
-                <article>9</article>
-                <article>9</article>
-                <article>9</article>
+                <?php   } ?>
             </section>
         </main>
         <script type="text/javascript">
